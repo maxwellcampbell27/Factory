afa39e96e8b03c0f13b324392029912d155c0b29
maxwellcampbell27 <maxwell_campbell27@milton.edu>
2025-11-07 08:39:45 -0500

    Added constructer

--- src/Factory.java
+++ src/Factory.java
@@ -1,10 +1,34 @@
 public class Factory {
+//todo: make a variable called "year" founded that is an integer
+    //instance variables at the top of class, they can be seen by every class
+    //they havwe to have the word public
+    public int  yearFounded = 1798;
+public String FactoryName = "airplane Factory";
+public boolean isOpen = true;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Max's Factory");
+ new Factory();
+    }
+
+    //constructor
+         public Factory(){
+        isOpen = false;
+        String manager = "Max";
+        System.out.println(manager);
+        factoryInfo();
+        //Todo: change the variable "yearfounded" to be a different value
+        // call factoryInf();
+    }
+        //make a method called factoryInfo, nothing should be in method
+    public void factoryInfo(){
+//todo: add souts to print out the 3 instance variables
+        System.out.println(yearFounded);
+        System.out.println(FactoryName);
+        System.out.println("It is"+ isOpen + "that the factory is open");
+        System.out.println(yearFounded + FactoryName + isOpen);
 
 
     }
-
 
 }
